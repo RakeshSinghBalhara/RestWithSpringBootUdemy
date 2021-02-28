94f543a79dbec38c5f02e377978a02629312a8a4
SINGH Rakesh CD5885 <CD5885@engie.com>
2021-02-28 13:12:11 +0530

    Hello App

---
+++ rest-with-springboot/src/main/java/com/balhara/learning/restwithspringboot/RestWithSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.balhara.learning.restwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringbootApplication {
+
+public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringbootApplication.class, args);
+	}
+}
---
+++ rest-with-springboot/src/test/java/com/balhara/learning/restwithspringboot/RestWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balhara.learning.restwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
