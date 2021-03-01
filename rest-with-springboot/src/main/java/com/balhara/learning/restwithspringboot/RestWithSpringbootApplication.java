219abd7ce20185a06f9bc4f971384adaaf9b75e7
SINGH Rakesh CD5885 <CD5885@engie.com>
2021-03-01 11:54:36 +0530

    CRUD opreation with MySQL DB Added.

--- rest-with-springboot/src/main/java/com/balhara/learning/restwithspringboot/RestWithSpringbootApplication.java
+++ rest-with-springboot/src/main/java/com/balhara/learning/restwithspringboot/RestWithSpringbootApplication.java
@@ -1,9 +1,15 @@
 package com.balhara.learning.restwithspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.balhara.learning.restwithspringboot.model"}) 
 public class RestWithSpringbootApplication {
 
 public static void main(String[] args) {
